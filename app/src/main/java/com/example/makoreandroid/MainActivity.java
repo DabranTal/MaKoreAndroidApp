7db496026db8e1975ba5f83a2a1ca8adcdb591f5
Tal Dabran <tal.dabran@live.biu.ac.il>
2022-06-11 21:05:17 +0300

    add adapter, viewmodel, entitie

--- app/src/main/java/com/example/makoreandroid/MainActivity.java
+++ app/src/main/java/com/example/makoreandroid/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.makoreandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.makoreandroid.adapters.MessageListAdapter;
+import com.example.makoreandroid.entities.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView  lstMessages= findViewById(R.id.recycler_conversaion);
+        final MessageListAdapter adapter = new MessageListAdapter(this);
+        lstMessages.setAdapter(adapter);
+        lstMessages.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message(0,"hi","20:41",true));
+        messages.add(new Message(1,"friend","20:41",true));
+        adapter.setMessages(messages);
+
     }
 }
---
+++ app/src/main/java/com/example/makoreandroid/adapters/MessageListAdapter.java
@@ -0,0 +1,66 @@
+package com.example.makoreandroid.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.makoreandroid.R;
+import com.example.makoreandroid.entities.Message;
+
+import java.util.List;
+
+public class MessageListAdapter extends RecyclerView.Adapter<MessageListAdapter.MessagesViewHolder> {
+
+    class MessagesViewHolder extends RecyclerView.ViewHolder {
+        private final EditText content;
+        private final EditText time;
+
+        private MessagesViewHolder(View itemView) {
+            super(itemView);
+            content = itemView.findViewById(R.id.content_sender_message);
+            time = itemView.findViewById(R.id.time_partner_message);
+
+        }
+    }
+
+    private final LayoutInflater mInflater;
+    private List<Message> messages;
+
+    public MessageListAdapter(Context context) {mInflater = LayoutInflater.from(context);}
+
+    @Override
+    public MessagesViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.sender_message, parent, false);
+        return new MessagesViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder (MessagesViewHolder holder, int position) {
+        if (messages != null) {
+            final  Message current = messages.get(position);
+            holder.content.setText(current.getContent());
+            holder.time.setText(current.getTime());
+        }
+    }
+
+    public void setMessages (List<Message> m) {
+        messages = m;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (messages != null) {
+            return messages.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public List<Message> getMessages() { return messages; }
+
+}
