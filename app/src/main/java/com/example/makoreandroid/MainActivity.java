88b5934333e078f4474f0c16eb821988bff1db3d
Tal Dabran <tal.dabran@live.biu.ac.il>
2022-06-11 23:41:55 +0300

    send Button add message to local list and clean typing board

--- app/src/main/java/com/example/makoreandroid/MainActivity.java
+++ app/src/main/java/com/example/makoreandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.makoreandroid;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.makoreandroid.adapters.MessageListAdapter;
 import com.example.makoreandroid.entities.Message;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
         messages.add(new Message(0, "hi", "20:40", true));
         messages.add(new Message(1, "friend", "20:42", true));
         adapter.setMessages(messages);
+
+        FloatingActionButton btnSend = findViewById(R.id.button_send);
+        btnSend.setOnClickListener(view->{
+            EditText et = findViewById(R.id.typing_board);
+            messages.add(new Message(3,et.getText().toString(),"21:40",true));
+            et.setText("");
+        });
 
     }
 }
