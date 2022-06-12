dadbb227ec3b4605fb27f042eac8d7b01c74d0c8
Coral Kuta <coral,kuta@live,biu.ac.il>
2022-06-12 09:18:58 +0300

    merge to master

--- app/src/main/java/com/example/makoreandroid/AppDB.java
+++ app/src/main/java/com/example/makoreandroid/AppDB.java
@@ -3,6 +3,8 @@ package com.example.makoreandroid;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.makoreandroid.entities.User;
+
 @Database(entities = {User.class}, version = 1)
 public abstract class AppDB extends RoomDatabase {
     public abstract UserDao userDao();
